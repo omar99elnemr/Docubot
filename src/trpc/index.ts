949e2b16b7dbd8d55c1cbca1dfb2f3963529856f
omar99elnemr <omar99elnemr@gmail.com>
2023-11-30 17:55:00 +0200

    Upgrade fix

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -129,44 +129,63 @@ export const appRouter = router({
       };
     }),
 
-  createStripeSession: privateProcedure.mutation(async ({ ctx }) => {
-    const { userId } = ctx;
-    const billingUrl = absoluteUrl("/dashboard/billing");
-
-    if (!userId) throw new TRPCError({ code: "UNAUTHORIZED" });
-    const dbUser = await db.user.findFirst({
-      where: {
-        id: userId,
-      },
-    });
-    if (!dbUser) throw new TRPCError({ code: "UNAUTHORIZED" });
-    const subscriptionPlan = await getUserSubscriptionPlan();
-    if (subscriptionPlan.isSubscribed && dbUser.stripeCustomerId) {
-      const stripeSession = await stripe.billingPortal.sessions.create({
-        customer: dbUser.stripeCustomerId,
-        return_url: billingUrl,
-      });
-
-      return { url: stripeSession.url };
-    }
-    const stripeSession = await stripe.checkout.sessions.create({
-      success_url: billingUrl,
-      cancel_url: billingUrl,
-      payment_method_types: ["card"],
-      mode: "subscription",
-      billing_address_collection: "auto",
-      line_items: [
-        {
-          price: PLANS.find((plan) => plan.name === "Pro")?.price.priceIds.test,
-          quantity: 1,
-        },
-      ],
-      metadata: {
-        userId: userId,
-      },
-    });
-    return { url: stripeSession.url };
-  }),
+    createStripeSession: privateProcedure.mutation(
+      async ({ ctx }) => {
+        const { userId } = ctx
+  
+        const billingUrl = absoluteUrl('/dashboard/billing')
+  
+        if (!userId)
+          throw new TRPCError({ code: 'UNAUTHORIZED' })
+  
+        const dbUser = await db.user.findFirst({
+          where: {
+            id: userId,
+          },
+        })
+  
+        if (!dbUser)
+          throw new TRPCError({ code: 'UNAUTHORIZED' })
+  
+        const subscriptionPlan =
+          await getUserSubscriptionPlan()
+  
+        if (
+          subscriptionPlan.isSubscribed &&
+          dbUser.stripeCustomerId
+        ) {
+          const stripeSession =
+            await stripe.billingPortal.sessions.create({
+              customer: dbUser.stripeCustomerId,
+              return_url: billingUrl,
+            })
+  
+          return { url: stripeSession.url }
+        }
+  
+        const stripeSession =
+          await stripe.checkout.sessions.create({
+            success_url: billingUrl,
+            cancel_url: billingUrl,
+            payment_method_types: ['card', 'paypal'],
+            mode: 'subscription',
+            billing_address_collection: 'auto',
+            line_items: [
+              {
+                price: PLANS.find(
+                  (plan) => plan.name === 'Pro'
+                )?.price.priceIds.test,
+                quantity: 1,
+              },
+            ],
+            metadata: {
+              userId: userId,
+            },
+          })
+  
+        return { url: stripeSession.url }
+      }
+    ),
 
   getFileUploadStatus: privateProcedure
     .input(z.object({ fileId: z.string() }))
