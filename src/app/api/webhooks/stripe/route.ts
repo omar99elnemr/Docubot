601ab672c691e9252a4b6192b0395b1489f32e3e
omar99elnemr <omar99elnemr@gmail.com>
2023-12-01 00:41:25 +0200

    Upgrade

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -24,7 +24,8 @@ export async function POST(request: Request) {
     )
   }
 
-  const session = event.data.object as Stripe.Checkout.Session
+  const session = event.data
+    .object as Stripe.Checkout.Session
 
   if (!session?.metadata?.userId) {
     return new Response(null, {
@@ -33,11 +34,11 @@ export async function POST(request: Request) {
   }
 
   if (event.type === 'checkout.session.completed') {
-    const subscription = await stripe.subscriptions.retrieve(
-      session.subscription as string
-    )
+    const subscription =
+      await stripe.subscriptions.retrieve(
+        session.subscription as string
+      )
 
-    // Update user data in the database
     await db.user.update({
       where: {
         id: session.metadata.userId,
@@ -55,11 +56,11 @@ export async function POST(request: Request) {
 
   if (event.type === 'invoice.payment_succeeded') {
     // Retrieve the subscription details from Stripe.
-    const subscription = await stripe.subscriptions.retrieve(
-      session.subscription as string
-    )
+    const subscription =
+      await stripe.subscriptions.retrieve(
+        session.subscription as string
+      )
 
-    // Update user data in the database
     await db.user.update({
       where: {
         stripeSubscriptionId: subscription.id,
