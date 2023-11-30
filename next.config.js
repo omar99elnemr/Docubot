8356fda3c9896cd4d8430ccec1d21b94a8f39ae9
omar99elnemr <omar99elnemr@gmail.com>
2023-11-30 03:51:00 +0200

    Try to fix image

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
 
     domains: [
-      'https://docubot-ten.vercel.app/',
+      'https://docubot-ten.vercel.app',
     ],
-
+    
     remotePatterns: [
       {
         protocol: "https",
