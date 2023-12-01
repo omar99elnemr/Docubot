8172aab9183f3863ac046056b6949ba301d5ed52
omar99elnemr <omar99elnemr@gmail.com>
2023-12-01 02:47:50 +0200

    Upgrade

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -4,7 +4,7 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
-  apiVersion: '2023-08-16',
+  apiVersion: '2023-10-16',
   typescript: true,
 })
 
