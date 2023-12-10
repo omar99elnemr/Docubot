a70800ce66555defa52685ef505424dabb692ba7
omar99elnemr <omar99elnemr@gmail.com>
2023-12-11 01:13:14 +0200

    Stripe fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,7 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest, NextResponse } from "next/server";
+import { authMiddleware } from '@kinde-oss/kinde-auth-nextjs/server'
 
-export async function middleware(req: NextRequest) {
-  const { isAuthenticated } = getKindeServerSession();
-  const isAuth = isAuthenticated();
-  if (!isAuthenticated()) {
-    const pathname = req.nextUrl.pathname; // relative pathname
-
-    const isApiRoute = req.nextUrl.pathname.startsWith("/api");
-    const unprotectedRoutes = ["/api/auth/login", "/api/auth/register", "/pricing", "/"];
-
-    if (!isAuth && !unprotectedRoutes.includes(pathname) && !isApiRoute) {
-      return NextResponse.redirect(new URL(`/api/auth/login`, req.url));
-    }
-  }
-}
 export const config = {
-  matcher: [
-    "/dashboard/:path*",
-    "/auth-callback",
-    "/((?!api|_next/static|_next/image|images|favicon.ico).*)",
-  ],
-};
+  matcher: ['/dashboard/:path*', '/auth-callback'],
+}
 
+export default authMiddleware
