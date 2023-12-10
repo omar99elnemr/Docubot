8421af5d8cfc67bf00056915a61b60051d6fee3d
omar99elnemr <omar99elnemr@gmail.com>
2023-12-11 00:42:47 +0200

    Stripe fix

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -2,8 +2,7 @@
 
 import { useRouter, useSearchParams } from 'next/navigation'
 import { trpc } from '../_trpc/client'
-import { useRef } from 'react'
-
+import { Loader2 } from 'lucide-react'
 
 const Page = () => {
   const router = useRouter()
@@ -11,39 +10,26 @@ const Page = () => {
   const searchParams = useSearchParams()
   const origin = searchParams.get('origin')
 
-  const retry = useRef(0);
-  const maxRetryCount = 3;
-
-  const { refetch } = trpc.authCallback.useQuery(undefined, {
+  trpc.authCallback.useQuery(undefined, {
     onSuccess: ({ success }) => {
       if (success) {
         // user is synced to db
-        router.push(origin ? `/${origin}` : "/dashboard");
+        router.push(origin ? `/${origin}` : '/dashboard')
       }
     },
     onError: (err) => {
-      console.log(err);
-      if (err.data?.code === "UNAUTHORIZED") {
-        retry.current = retry.current + 1;
-        if (retry.current <= maxRetryCount) {
-          // Retry up to maxRetryCount
-          setTimeout(() => {
-            refetch();
-          }, 500);
-        } else {
-          router.push("/sign-in");
-        }
+      if (err.data?.code === 'UNAUTHORIZED') {
+        router.push('/sign-in')
       }
     },
-
-    retry: false,
+    retry: true,
     retryDelay: 500,
-  });
+  })
 
   return (
     <div className='w-full mt-24 flex justify-center'>
       <div className='flex flex-col items-center gap-2'>
-       
+        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
         <h3 className='font-semibold text-xl'>
           Setting up your account...
         </h3>
