bf87bd052bf18b36623d98ab955a933b7906c7c7
omar99elnemr <omar99elnemr@gmail.com>
2023-12-11 02:35:33 +0200

    Skeleton fix

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -54,7 +54,7 @@ function Dashboard({subscriptionPlan} : PageProps ) {
                   className="flex flex-col gap-2"
                 >
                   <div className="pt-6 px-6 flex w-full items-center justify-between space-x-6">
-                    <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
+                    <div className="h-10 w-10 flex-shrink-0 rounded-full bg-[url(`https://robohash.org/${file.id}.png`)]" />
                     <div className="flex-1 truncate">
                       <div className="flex items-center space-x-3">
                         <h3 className="truncate text-lg font-medium text-zinc-900">
