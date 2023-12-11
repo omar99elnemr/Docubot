d4335409f2fee83eade7eb294663665a95a4814f
omar99elnemr <omar99elnemr@gmail.com>
2023-12-11 03:45:44 +0200

    Favicon-middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,10 @@
 import { authMiddleware } from '@kinde-oss/kinde-auth-nextjs/server'
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/auth-callback'],
+  matcher: ['/dashboard/:path*',
+              '/auth-callback',
+              "/((?!api|_next/static|_next/image|images|favicon.ico).*)"
+            ],
 }
 
 export default authMiddleware
