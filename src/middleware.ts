e7f0c807484a03a62d6b8f892d4bdc22fccdb74c
omar99elnemr <omar99elnemr@gmail.com>
2023-12-11 04:09:25 +0200

    Favicon-middleware-rollback

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,7 @@
 import { authMiddleware } from '@kinde-oss/kinde-auth-nextjs/server'
 
 export const config = {
-  matcher: ['/dashboard/:path*',
-              '/auth-callback',
-              "/((?!api|_next/static|_next/image|images|favicon.ico).*)"
-            ],
+  matcher: ['/dashboard/:path*','/auth-callback' ],
 }
 
 export default authMiddleware
