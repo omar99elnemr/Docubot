d803cb1c1e6198b757f5aa3340203d307080a6f1
omar99elnemr <omar99elnemr@gmail.com>
2023-12-14 01:06:09 +0200

    fixes

--- src/app/_trpc/client.ts
+++ src/app/_trpc/client.ts
@@ -1,5 +1,5 @@
 import { AppRouter } from "@/trpc";
 import { createTRPCReact } from "@trpc/react-query";
 
-
+// Create the TRPC client with the typed router
 export const trpc = createTRPCReact<AppRouter>({})
--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,7 +1,10 @@
+// This file defines the server-side entry point for handling TRPC requests.
+
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { appRouter } from '@/trpc';
 
 const handler = (req: Request) =>
+ // This function parses the request and executes the corresponding TRPC procedure based on the method and headers.
   fetchRequestHandler({
     endpoint: '/api/trpc',
     req,
@@ -9,4 +12,6 @@ const handler = (req: Request) =>
     createContext: () => ({})
   });
 
+
+// Exports the `handler` function as both GET and POST, enabling it to handle both request methods.
 export { handler as GET, handler as POST };
